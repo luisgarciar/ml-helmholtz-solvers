7e5e4e0eb53c2dcd8e63b1ac6162ab2c17842a95
luisgarciar <luisgarciar@gmail.com>
2016-10-10 16:04:13 +0200

    modified fem test functions and mg setup functions

---
+++ FEM/mgsolverfem/npc_numlev_to_npf_fem.m
@@ -0,0 +1,26 @@
+function [npf]= npc_numlev_to_npf_fem(npc,numlev)
+%% NPC_NUMLEV_TO_NPF Computes the number of points in a fine grid
+% given the number of points in the coarsest grid and number of level
+% 
+%  Use:
+%  [npf] = npc_numlev_to_npf_fem(npc,numlev)
+%
+%  Input: 
+%  npc:    number of points on coarsest grid (1D)
+%          (x=0 and interior points)
+%
+%  numlev: number of levels (grids) including coarsest and finest
+%
+%  Output:
+%  npf:   number of points on finest grid (1D)
+%
+% Important: 
+%
+%  Author:      Luis Garcia Ramos, 
+%               Institut fur Mathematik, TU Berlin
+%               Version 2.0, Oct 2016
+
+npf = 2^(numlev-1)*(npc);
+
+end
+
