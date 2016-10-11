9882b77476ce9531e3737a37f6c7c6dbd20e2aca
luisgarciar <luisgarciar@gmail.com>
2016-10-11 15:03:28 +0200

    wrote conclusions for experiment file

--- FD/tests_&_examples/test_mg_gmres_dir_vs_fem_1D.m
+++ FD/tests_&_examples/test_mg_gmres_dir_vs_fem_1D.m
@@ -9,16 +9,15 @@
 %Using a point source on the right hand side, the Sommerfeld problem
 %requires fewer iterations (as remarked by the experiments of several authors)
 
-
 clear global; 
 close all;
 clc;
 
-%% Parameters
-k        = 200;      %wavenumber
-ppw      = 20;       %min points per wavelength%
+% Parameters
+k        = 40;   %wavenumber
+ppw      = 30;    %min points per wavelength%
 npc_int  = 3;     %number of interior points in coarsest grid
-dim      = 1;        %dimension
+dim      = 1;     %dimension
 
 % Parameters of multigrid solver
 npre = 2; npos = 1; w = 2/3; smo = 'wjac';
@@ -40,21 +39,24 @@ profile on
 eps = 0.5*k^2;
 [mg_mat_fem,mg_split_fem,restrict_fem,interp_fem] = mg_setupfem(k,eps,op_type,npc_fem,numlev,dim);
 
-%%
-% right hand side and initial guess
-M_fem  = mg_mat_fem{1}; 
+%right hand side and initial guess
+M_fem  = mg_mat_fem{1};
+
+%constant right hand side
 %rhs_fem  = ones(npf_fem,1); rhs_fem(npf_fem) = 0.5; h = 1/npf_fem; rhs_fem = h*rhs_fem; %(constant function f=1)
+
+%point source on right hand side
 rhs_fem = ones(npf_fem,1); rhs_fem(npf_fem/2)=h*1;
 x0_fem = zeros(size(rhs_fem));
 
 
 %% Multigrid comparison
 
-%Mixed Problem
+%Mixed Problem, FEM
 
 %multigrid cycle for shifted Laplacian
 numcycles = 12;
-relres_ = zeros(numcycles+1,1); relres_fem_mg(1)=norm(rhs_fem-M_fem*x0_fem); 
+relres_fem_mg = zeros(numcycles+1,1); relres_fem_mg(1)=norm(rhs_fem-M_fem*x0_fem); 
 %relerr_fem = zeros(numcycles+1,1); %relerr_fem(1)=norm(ex_sol_dir);
 
 for i=1:numcycles
@@ -73,15 +75,21 @@ bc = 'dir';
 % Multigrid setup for shifted Laplacian
 [mg_mat_dir,mg_split_dir,restrict_dir,interp_dir] = mg_setup(k,eps,op_type,npc_int,numlev,bc,dim);
 
-%Matrix, right hand side and initial guess
-M_dir =  mg_mat_dir{1}; 
+% Matrix, right hand side and initial guess
+M_dir =  mg_mat_dir{1};
+
+% constant right hand side
+%rhs_dir =  ones(length(M_dir),1); 
+
+% point source
 rhs_dir =  zeros(length(M_dir),1);  rhs_dir(floor(npf/2))=1;
 x0 =  rand(length(M_dir),1); 
 
-%Setup of residuals and error
+% Setup of residuals and error
 relres_mg_dir = zeros(numcycles+1,1); relres_mg_dir(1) = norm(rhs_dir-M_dir*x0); 
 %relerr_dir = zeros(numcycles+1,1); relerr_dir(1) = norm(ex_sol_dir);
 
+% Multigrid cycle of Dirichlet problem
 tic
 for i=1:numcycles
     [x_sol] = Vcycle(mg_mat_dir,mg_split_dir,restrict_dir,interp_dir,x0,rhs_dir,npre,npos,w,smo,1);
@@ -116,22 +124,22 @@ x0 = zeros(length(A_dir),1);
  
 
 %% Test of Preconditioned GMRES
-%Setting the MG preconditioner Minv
-%Parameters of V-cycle and Jacobi iteration
+% Setting the MG preconditioner Minv
+% Parameters of V-cycle and Jacobi iteration
 npre = 2; npos = 1; w = 2/3; smo = 'wjac'; numcycles = 1;
 Minv_dir = @(v) feval(@Vcycle,mg_mat,mg_split,restrict,interp,x0,v,npre,npos,w,smo,1);
 AMinv_dir = @(v) A_dir*feval(Minv_dir,v);
 
-%Parameters of GMRES iteration
+% Parameters of GMRES iteration
 tol   = 1e-10;
 maxit = min(150,length(A_dir));
  
-%GMRES iteration with left SL preconditioner
+% GMRES iteration with left SL preconditioner
 tic
 [x_lgdir,flag_lgdir,relres_lgdir,iter_lgdir,resvec_lgdir] = gmres(A_dir,rhs_dir,[],tol,maxit,Minv_dir);
 time_lgdir = toc;
 
-%GMRES iteration with right SL preconditioner
+% GMRES iteration with right SL preconditioner
 tic
 [x_rgdir,flag_rgdir,relres_rgdir,iter_rgdir,resvec_rgdir] = gmres(AMinv_dir,rhs_dir,[],tol,maxit);
 time_rgdir = toc;
@@ -142,11 +150,11 @@ numcycles = 1;
 Minv_fem = @(v) feval(@Vcycle,mg_mat_fem,mg_split_fem,restrict_fem,interp_fem,x0,v,npre,npos,w,smo,numcycles);
 AMinv_fem = @(v) A_fem*feval(Minv_fem,v);
  
-% %GMRes parameters
-tol   = 1e-7;
+% GMRes parameters
+tol   = 1e-10;
 maxit = min(150,size(A_fem,1));
 
-%GMRES + left preconditioning with MG - Shifted Laplacian  
+% GMRES + left preconditioning with MG - Shifted Laplacian  
 tic
 [x_lgfem,flag_lgfem,relres_lgfem,iter_lgfem,resvec_lgfem] = gmres(A_fem,rhs_fem,[],tol,maxit,Minv_fem);
 time_lgfem = toc;
@@ -161,15 +169,20 @@ iter_lgdir
 iter_rgfem
 iter_rgdir
 
+time_lgfem
+time_rgfem
+
+time_lgdir
+time_rgdir
+
 
 %% Plots of GMRES results
- figure(1)
- semilogy(0:iter_rgfem(2), resvec_rgfem/resvec_rgfem(1), 'k-');
- hold on
- semilogy(0:(length(resvec_rgdir)-1), resvec_rgdir/resvec_rgdir(1), 'b-');
- ylabel('relative residual')
- xlabel('iteration')
+figure(1)
+semilogy(0:iter_rgfem(2), resvec_rgfem/resvec_rgfem(1), 'k-');
+hold on
+semilogy(0:(length(resvec_rgdir)-1), resvec_rgdir/resvec_rgdir(1), 'b-');
+ylabel('relative residual')
+xlabel('iteration')
  
-% legend('Sommerfeld BCs', 'Dirichlet BCs')
-% title(['1D Helmholtz with CSL-preconditioner (k=',num2str(k),')'])
-% 
+legend('FEM problem with mixed BCs', 'FD problem with Dirichlet BCs')
+title(['Solving the Helmholtz 1D equation with GMRES and CSL-preconditioner (k=',num2str(k),')'])
