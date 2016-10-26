2435bbc2788e47255acd428d0086c117f35bd9b3
luisgarciar <luisgarciar@gmail.com>
2016-10-26 15:05:47 +0200

    Finished new Vcycle with livshits smoothing

--- FD/mg_solver/Vcycle.m
+++ FD/mg_solver/Vcycle.m
@@ -33,25 +33,23 @@ function [x_sol] = Vcycle(mg_mat,mg_split,restrict,interp,x0,b,npre,npos,w,smo,n
         x_sol = x0;
         
         for i=1:numcycles
+            
             %Presmoothing and computation of the residual
             %fprintf('Presmoothing with matrix of size %d\n',length(grid_matrices{1,1}));
-            
             %Special case: Kaczmarcz relaxation
             if (length(mg_split{1})>4 && strcmp(smo,'liv')) 
                 %Gauss-Seidel applied to normal equations
                 rhs = mg_mat{i}'*b;
                 x_sol = smoother(mg_split{1}.Uk, mg_split{1}.Lk,...
-                       mg_split{1}.Dk, mg_split{1}.P,rhs,x_sol,w,4,'gs');
-                   
-            else
-                
+                       mg_split{1}.Dk, mg_split{1}.P,rhs,x_sol,w,4,'gs');                   
+            elseif strcmp(smo,'liv')
                 x_sol = smoother(mg_split{1}.U, mg_split{1}.L,...
-                        mg_split{1}.D, mg_split{1}.P,b,x_sol,w,npre,smo);
-            
-                
+                        mg_split{1}.D, mg_split{1}.P,b,x_sol,w,npre,'gs');            
+            else
+               x_sol = smoother(mg_split{1}.U, mg_split{1}.L,...
+                        mg_split{1}.D, mg_split{1}.P,b,x_sol,w,npre,'smo');
             end
             
-                         
             res   = b-mg_mat{1}*x_sol;
 
             %Restriction of the residual to coarse grid
@@ -72,9 +70,22 @@ function [x_sol] = Vcycle(mg_mat,mg_split,restrict,interp,x0,b,npre,npos,w,smo,n
             %Prolongation and correction on current grid
             x_sol = x_sol + interp{1}*vc;
         
-            %Postsmoothing
-            x_sol = smoother(mg_split{1}.U,mg_split{1}.L,...
-                             mg_split{1}.D,mg_split{1}.P,b,x_sol,w,npos,smo);           
+            %Postsmoothing  
+            if (length(mg_split{1})>4 && strcmp(smo,'liv')) 
+                %4 its of Gauss-Seidel applied to normal equations
+                rhs = mg_mat{i}'*b;
+                x_sol = smoother(mg_split{1}.Uk, mg_split{1}.Lk,...
+                       mg_split{1}.Dk, mg_split{1}.P,rhs,x_sol,w,4,'gs');
+                   
+            elseif strcmp(smo,'liv')
+                x_sol = smoother(mg_split{1}.U, mg_split{1}.L,...
+                        mg_split{1}.D, mg_split{1}.P,b,x_sol,w,npos,'gs');            
+            else
+               x_sol = smoother(mg_split{1}.U, mg_split{1}.L,...
+                        mg_split{1}.D, mg_split{1}.P,b,x_sol,w,npos,'smo');
+            end
+            
+                         
         end
     end   
 
