1bec9d105d832a6fecd2c5e814518322a2c64b07
luisgarciar <luisgarciar@gmail.com>
2016-12-07 13:35:43 +0100

    added option for parameter less than 1

--- FEM/mgsolverfem/fem_npc_to_npf.m
+++ FEM/mgsolverfem/fem_npc_to_npf.m
@@ -1,4 +1,4 @@
-function [npf,lev] = fem_npc_to_npf(npc,k,ppw)
+function [npf,lev] = fem_npc_to_npf(npc,k,par)
 %% NPC_TO_NPF Computes the number of points on the finest grid given the
 %         number of points on the coarsest grid and discretization
 %         requirements
@@ -19,12 +19,27 @@ function [npf,lev] = fem_npc_to_npf(npc,k,ppw)
 %               Version 1.0, Jun 2016
 
 %%  
-
+%Case 1: The number of fine grid points grows linearly with the wavenumber
+%In this case par equals the number of gridpoints per wavelength
 %The number of grid levels lev and points of the fine grid npf is
-%chosen according to the rule (2*pi/k*npf) approx ppw
-m   = ppw*k/(2*pi*npc);
-lev = ceil(log2(m));
-%lev = max(lev,1);   %at least 1 level
-npf = npc*(2^(lev-1));
+%chosen according to the rule (2*pi/k*npf) approx par=ppw
+if par >1
+    ppw = par;
+    m   = ppw*k/(2*pi*npc);
+    lev = ceil(log2(m));
+    lev = max(lev,1);   %at least 1 level
+    npf = npc*(2^(lev-1));
+    
+else 
+%Case 2: The number of fine grid points grows 
+%linearly w.r.t. k^2. In this case par <<1, 
+%and the gridsize h=1/(npf+1) satisfies k^2*h approx par
+    m  = (k^2/par)*(1/(npc));
+   lev = ceil(log2(m));
+   lev = max(lev,1);   %at least 1 level
+   npf = npc*(2^(lev-1));
+    
+end
+
 end
 
