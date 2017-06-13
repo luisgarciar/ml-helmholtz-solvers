a3abf0861bcfa12d247d1aef6a08af6bc54c8250
luisgarciar <luisgarciar@gmail.com>
2017-06-13 14:14:31 +0200

    Added Sommerfeld boundary conditions to mass matrix in 1D

--- FEM/matrix_constructors/helmholtzfem.m
+++ FEM/matrix_constructors/helmholtzfem.m
@@ -8,18 +8,28 @@ function [A] = helmholtzfem(k,np,eps,bc)
 %        u(0)       = 0   
 %        u'(1)-ku(1)= 0
 %
-%       Sommerfeld bc
+%       Sommerfeld bc:
 %       -u''- k^2*u-i*eps*u = f in (0,1)
 %        u'(0) + ku(0) = 0   
 %        u'(1) - ku(1) = 0
 %
-% 
-% Use: A = helmholtzfem(k,np,eps)
+%  Use: A = helmholtzfem(k,np,eps)
+%
+%  Input: 
+%  k:   wavenumber
+%  np:  size of the matrix (only interior points)
+%  eps: imaginary shift    (eps=0 for Helmholtz problem)
+%  bc: 'mix' or 'som'
+%  
+%  Output:
+%  A:   FEM discretization matrix
+%       size(A) = (np+2,np+2) if bc = 'som'  
+%       size(A) = (np+1,np+1) if bc = 'mix'   
 %
 % Note (For shifted Laplacian problems): 
 % The imaginary shift is not multiplied by the term k^2
 % as in the original publications of Erlangga et. al.
-% For a convergent multigrid solver one needs eps~k^2 (eps=0.5k^2)
+% For a convergent multigrid solver one needs eps~k^2 (e.g. eps = 0.5k^2)
 % For wave-independent GMRES convergence one needs eps~k 
 % (but the MG solver will not converge)
 %
@@ -29,35 +39,29 @@ function [A] = helmholtzfem(k,np,eps,bc)
 %
 % S Langdon, S Chandler-Wilde, Finite element methods for acoustic scattering 
 % 
-%  Input: 
-%  k:   wavenumber
-%  np:  size of the matrix (only interior points)
-%  eps: imaginary shift    (eps=0 for Helmholtz problem)
-%  
-%  Output:
-%  A:   FEM discretization matrix
-%       size(A) = (np+2,np+2) for if bc = 'som'  
-%       size(A) = (np+1,np+1) for if bc = 'mix'  
-%
 %  Author: Luis Garcia Ramos, 
 %          Institut fur Mathematik, TU Berlin
-%  Version 0.1 - Jun 2016
-%
-%%%
-
-%%
+%  Version 0.2 - Jun 2017
+%%%%
 
 switch bc
-    case 'mix'
+    case 'mix' %mixed boundary conditions
         h  = 1/(np+1);                 %gridsize
-        e = ones(np+1,1);
-        A = (1/h)*spdiags([-e 2*e -e],[-1 0 1],np+1,np); A(np+1,np+1)=1/h; %Laplacian Matrix
-        B = h*spdiags([(1/6)*e (2/3)*e (1/6)*e],[-1 0 1],np+1,np+1); B(np+1,np+1)=h/3; %Mass matrix
-        C = sparse(np+1,np+1); C(np+1,np+1)=1;  %Boundary term
-        A = A-(k^2+1i*eps)*B-1i*k*C;        %Helmholtz matrix
+        e  = ones(np+1,1);
+        A  = (1/h)*spdiags([-e 2*e -e],[-1 0 1],np+1,np+1); A(np+1,np+1)=1/h; %Laplacian Matrix
+        B  = h*spdiags([(1/6)*e (2/3)*e (1/6)*e],[-1 0 1],np+1,np+1); B(np+1,np+1)=h/3; %Mass matrix
+        C  = sparse(np+1,np+1); C(np+1,np+1)=1;  %Boundary term
+        A  = A-(k^2+1i*eps)*B-1i*k*C;            %Helmholtz matrix
 
-    case 'som'
-
+    case 'som' %sommerfeld boundary conditions
+         h  = 1/(np+1);                 %gridsize
+        e  = ones(np+2,1);
+        A  = (1/h)*spdiags([-e 2*e -e],[-1 0 1],np+2,np+2); 
+        A(1,1)=1/h; A(np+2,np+2)=1/h;       %Laplacian Matrix
+        B  = h*spdiags([(1/6)*e (2/3)*e (1/6)*e],[-1 0 1],np+2,np+2); 
+        B(1,1) = h/3; B(np+2,np+2)=h/3;     %Mass matrix
+        C  = sparse(np+2,np+2); C(1,1)=1; C(np+2,np+2)=1;  %Boundary term
+        A  = A-(k^2+1i*eps)*B-1i*k*C;            %Helmholtz matrix
 end
 
 
--- FEM/matrix_constructors/mass.m
+++ FEM/matrix_constructors/mass.m
@@ -1,13 +1,31 @@
-function [M] = mass(np)
+function [M] = mass(np,bc)
 %% MASS: Constructs a mass matrix for the 1D Helmholtz model problem.
-%  Constructs the mass corresponding to the discretization of a problem
-%  with P1 finite elements in (0,1). The model problem is given by
+%  Constructs the mass matrx corresponding to the discretization of a 
+%  Helmholtz problem  with P1 finite elements in (0,1) with mixed or 
+%  Sommerfeld boundary conditions. The model problem is given by
 %
-%       -u''- k^2*u-i*eps*u = f in (0,1)
-%        u(0)       = 0   
-%        u'(1)-ku(1)= 0
+%  Mixed bc:
+%  - u''- k^2*u-i*eps*u = f in (0,1)
+%  u(0)        = 0   
+%  u'(1)-ku(1) = 0
+%
+%  Sommerfeld bc:
+%  - u''- k^2*u-i*eps*u = f in (0,1)
+%    u'(0) + ku(0) = 0   
+%    u'(1) - ku(1) = 0
 % 
-% Use: A = mass(np)
+%  - u''- k^2*u-i*eps*u = f in (0,1)
+%    u(0)        = 0   
+%    u'(1)-ku(1) = 0
+% 
+% Use: M  = mass(np,bc)
+%      np:  number of interior points
+%      bc: 'mix' or 'som'
+%
+% Output:
+%   M: mass matrix
+%   size(M) = (np+2,np+2) if bc = 'som'  
+%   size(M) = (np+1,np+1) if bc = 'mix' 
 %
 % References:
 % F. Ihlenburg and I. Babuska, Finite element solution of the 
@@ -15,21 +33,28 @@ function [M] = mass(np)
 %
 % S Langdon , S Chandler-Wilde, Finite element methods for acoustic scattering 
 % 
-%  Input: 
-%  np:  size of the matrix (x=0 and interior points)
-%  
-%  Output:
-%  M:   Mass matrix
-%
 %  Author: Luis Garcia Ramos, 
 %          Institut fur Mathematik, TU Berlin
-%  Version 0.1 - Jun 2016
+%  Version 0.2 - Jun 2017
 %
 
 %%
-h = 1/np;            %gridsize
-e = ones(np,1);
-M = h*spdiags([(1/6)*e (2/3)*e (1/6)*e],[-1 0 1],np,np); M(np,np)=h/3; %Mass matrix
+h = 1/(np+1);   %gridsize
+switch bc
+    case 'mix' %mixed boundary conditions
+        e = ones(np+1,1);
+        M = h*spdiags([(1/6)*e (2/3)*e (1/6)*e],[-1 0 1],np+1,np+1);
+        M(np+1,np+1)=h/3; %Mass matrix
+        
+    case 'som' %Sommerfeld boundary conditions
+        e = ones(np+2,1);
+        M = h*spdiags([(1/6)*e (2/3)*e (1/6)*e],[-1 0 1],np+2,np+2);
+        M(1,1) = h/3;
+        M(np+2,np+2) = h/3; %Mass matrix    
+    
 end
+
+end
+
 
 
