ec348b43c1fe12723ebcd97dc454b32e5a4de6be
luisgarciar <luisgarciar@gmail.com>
2017-08-15 14:23:47 +0200

    1-Finished jupyter notebook with symbolic computations for the plane wave problem 2-Finished function to generate data for the plane wave test problem    Added boundary condition

--- FEM/matrix_constructors/test_ifem.m
+++ FEM/matrix_constructors/test_ifem.m
@@ -8,42 +8,39 @@ npf = ceil(k^(3/2));
         npf = npf+1;
     end
     npc = (npf-1)/2;
+   
+for jj = 1:length(scale)
     
-
-scale = 2.^(2:2:8);    
-
-
-    for jj = 1:length(scale)
-
     poweps    = 2;
     factoreps = 1;
     bc = 'som';
-
+    
     %Construct square mesh of meshsize h
     h = 1/npf;
     [node,elem] = squaremesh([0,1,0,1],h);
-
+    
     %Visualize mesh, nodes, elements
     showmesh(node,elem)
     hold on;
     findnode(node,1:length(node));% find node indices
-
+    
     %Find boundary nodes
-    [bdNode,bdEdge,isBdNode] = findboundary(elem); 
-
+    [bdNode,bdEdge,isBdNode] = findboundary(elem);
+    
     %Sets Sommerfeld boundary conditions on all boundary edges
-    bdFlag = setboundary(node,elem,'ABC'); 
-
+    bdFlag = setboundary(node,elem,'ABC');
+    
     %the structure pde contains data for a simple test problem
+    t = pi/2;
     pde = helmholtz2Dtrigdata(k);
-
+    
     [eqn,info] = helmholtz2Dfem(node,elem,pde,bdFlag);
-
+    
     %Matrix and right hand side
     A=eqn.A; b=eqn.b;
     %b = zeros(length(A),1); b(ceil(length(A)/2),1)=1;
     u = A\b;
     u_exact = pde.exactu(node);
-
+    
     showsolution(node,elem,u);
-    end
+end
