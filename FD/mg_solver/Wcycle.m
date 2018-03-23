1f51e202b418f7d412922506e8a6eaa028b5b168
luisgarciar <luisgarciar@gmail.com>
2018-03-23 10:26:07 +0100

    Fixed Wcycle

--- FD/mg_solver/Wcycle.m
+++ FD/mg_solver/Wcycle.m
@@ -58,11 +58,15 @@ function [x_sol] = Wcycle(mg_mat,mg_split,restrict,interp,x0,b,npre,npos,w,smo,n
         
             %Calling Wcycle to solve the error equation
             if(levs >2)
-                vc  = Wcycle(mg_mat(2:levs),mg_split(2:levs),restrict(2:levs-1),interp(2:levs-1),vc,fc,npre,npos,w,smo,2);
+                vc  = Wcycle(mg_mat(2:levs),mg_split(2:levs),restrict(2:levs-1),interp(2:levs-1),vc,fc,npre,npos,w,smo,1);
+                vc  = Wcycle(mg_mat(2:levs),mg_split(2:levs),restrict(2:levs-1),interp(2:levs-1),vc,fc,npre,npos,w,smo,1);
+
                 %size(fc)
                 
             elseif(levs==2)
-                vc = Wcycle(mg_mat(2:2),mg_split(2:2),restrict(1:1),interp(1:1),vc,fc,npre,npos,w,smo,2);
+                vc = Wcycle(mg_mat(2:2),mg_split(2:2),restrict(1:1),interp(1:1),vc,fc,npre,npos,w,smo,1);
+                vc = Wcycle(mg_mat(2:2),mg_split(2:2),restrict(1:1),interp(1:1),vc,fc,npre,npos,w,smo,1);
+
                 %size(fc)
             end
         
